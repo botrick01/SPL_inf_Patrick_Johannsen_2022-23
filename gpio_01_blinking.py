bf225368d814963c47191423a80aa59c1729f802
botrick01 <johannsenpatrick.privat@gmail.com>
2017-11-14 10:52:46 +0100

    blinking 1

---
+++ gpio_01_blinking.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+def setup():
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(2,GPIO.OUT)
+def loop():
+        GPIO.output(2,True)
+        time.sleep(1)
+        GPIO.output(2,False)
+        time.sleep(1)
+setup()
+while True:
+        loop()
